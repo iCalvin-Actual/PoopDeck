dee6ca05b06a90a356f96d8a127eb1d313d73578
Calvin Chestnut <me@icalvin.dev>
2020-05-02 17:08:04 -0400

    Initial Commit

---
+++ BabyTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabyTracker
+//
+//  Created by Calvin Chestnut on 5/2/20.
+//  Copyright © 2020 Calvin Chestnut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
